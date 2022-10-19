a62e1f09d0cb78273a3c0875740d3eb658bd6b99
Business Tech Academy <114942117+BusinessTechAcademy@users.noreply.github.com>
2022-10-19 09:16:22 -0700

    Update documented_code.py

--- expiremental/documented_code.py
+++ expiremental/documented_code.py
@@ -2,84 +2,150 @@
 #
 # SPDX-License-Identifier: MIT
 
-import time # imported for feed selection & delays
-import random # imported for feed selection
-import board # imported for basic board functions
-import terminalio # imported for label and fonts
+# Quote board matrix display
+# uses AdafruitIO to serve up a quote text feed and color feed
+# random quotes are displayed, updates periodically to look for new quotes
+# avoids repeating the same quote twice in a row
+
+import time
+import random
+import board
+import terminalio
 from adafruit_matrixportal.matrixportal import MatrixPortal
 
-# Setting up display
+# --- Display setup ---
 matrixportal = MatrixPortal(status_neopixel=board.NEOPIXEL, debug=True)
 
-# Creating the label
+# Create a new label with the color and text selected
 matrixportal.add_text(
-    text_font=terminalio.FONT, # Applying the font
-    text_position=(2, (matrixportal.graphics.display.height // 2) - 1), #Adjusting label height
-    scrolling=True, # Applying text scrolling
+    text_font=terminalio.FONT,
+    text_position=(0, (matrixportal.graphics.display.height // 2) - 1),
+    scrolling=True,
 )
 
-QUOTES_FEED = "sign-quotes.signtext" # Get the information from adafruit io
-COLORS_FEED = "sign-quotes.signcolor" # Example format: "group.feed"
-SCROLL_DELAY = 0.02 # Speed at which text scrolls at
-UPDATE_DELAY = 600 # Speed at which data is pulled from the feeds
+# Static 'Connecting' Text
+matrixportal.add_text(
+    text_font=terminalio.FONT,
+    text_position=(2, (matrixportal.graphics.display.height // 2) - 1),
+)
 
-quotes = [] # Setting up arrays: colors and quotes
-colors = [] # Will pull from the feeds in the adafruit IO
-last_color = None # Creating variables: last_quote and last_color
-last_quote = None # These variables are used so that the feed doesn't pull the same value randomly
+ANIM_FEED = "sign-quotes.signanim" 
+QUOTES_FEED = "sign-quotes.signtext"
+COLORS_FEED = "sign-quotes.signcolor"
+SCROLL_DELAY = 0.02
+UPDATE_DELAY = 600
+ANIM_STAGE = -1
 
-# Function used to update data to the matrixportal
+quotes = []
+colors = []
+anims = []
+anim_state = None
+last_color = None
+last_quote = None
+
+
 def update_data():
-    matrixportal.set_text("Connecting", 1) # Drawing "Connecting" on the matrix to show that the update has started
+    print("Updating data from Adafruit IO")
+    matrixportal.set_text("Connecting", 1)
 
     try:
-        quotes_data = matrixportal.get_io_data(QUOTES_FEED) # Gathering quotes from the adafruit io
-        quotes.clear() # Only pulling one quote from the feed
-        for json_data in quotes_data:
-            quotes.append(matrixportal.network.json_traverse(json_data, ["value"])) # Making data applicable to matrixportal
-    except Exception as error: # Telling it to stop updating if there is a connection error
-        matrixportal.set_text(" error ", 1) # Drawing "error" on the matrix to show that an error has occured
+        quotes_data = matrixportal.get_io_data(QUOTES_FEED)
+        quotes.clear()
+        for json_data in quotes_data:z
+            quotes.append(matrixportal.network.json_traverse(json_data, ["value"]))
+        print(quotes)
+    # pylint: disable=broad-except
+    except Exception as error:
+        print(error)
 
     try:
-        color_data = matrixportal.get_io_data(COLORS_FEED) # Gathering colors from the adafruit io
-        colors.clear() # Only pulling one color from the feed
+        color_data = matrixportal.get_io_data(COLORS_FEED)
+        colors.clear()
         for json_data in color_data:
-            colors.append(matrixportal.network.json_traverse(json_data, ["value"]))# Making data applicable to matrixportal
-    except Exception as error: # Telling it to stop updating if there is a connection error
-        matrixportal.set_text(" error ", 1) # Drawing "error" on the matrix to show that an error has occured
+            colors.append(matrixportal.network.json_traverse(json_data, ["value"]))
+        print(colors)
+    # pylint: disable=broad-except
+    except Exception as error:
+        print(error)
+        
+    try:
+        anim_data = matrixportal.get_io_data(QUOTES_FEED)
+        anims.clear()
+        for json_data in anim_data:
+            anim.append(matrixportal.network.json_traverse(json_data, ["value"]))
+        print(anim)
+    # pylint: disable=broad-except
+    except Exception as error:
+        print(error)
 
-    if not quotes: # If there are no quotes in the feeds or the feed doesnt exist display an error
-        raise RuntimeError("Please add at least one quote to your feeds")
-    matrixportal.set_text(" No Quotes! ", 1) # Drawing "No Quotes!" on the matrix to show that there is a problem with the quotes
-    if not colors: # If there are no color in the feeds or the feed doesnt exist display an error
-        raise RuntimeError("Please add at least one color to your feeds")
-    matrixportal.set_text(" No Colors! ", 1) # Drawing "No Colors!" on the matrix to show that there is a problem with the quotes
+    if not quotes or not colors or not anim:
+        raise RuntimeError("Please add at least one quote and color to your feeds")
+    matrixportal.set_text("missing data", 1)
 
 
-update_data() # Running the update function
-last_update = time.monotonic() # Updating time to monotonic so scrolling and delays are consistent
-matrixportal.set_text(" ", 1) # Between updates clear the text & creates blinking effect
-quote_index = None # Telling us that the update function should be ran because there is no current quote or color
+update_data()
+last_update = time.monotonic()
+matrixportal.set_text(" ", 1)
+quote_index = None
 color_index = None
 
 while True:
-
-    if len(quotes) > 1 and last_quote is not None: # Choosing a random quote
-        while quote_index == last_quote: # Active when the data is pulled and is the same as the last pull
-            quote_index = random.randrange(0, len(quotes)) # Forces another pull from the feed to prevent the same text from displaying twice
+    # Choose a random quote from quotes
+    if len(quotes) > 1 and last_quote is not None:
+        while quote_index == last_quote:
+            quote_index = random.randrange(0, len(quotes))
     else:
-        quote_index = random.randrange(0, len(quotes)) # Used if the same quote isn't pulled and runs normally
-    last_quote = quote_index # Telling us that a quote has be chosen and not to pick it twice by changing the value of last_quote to it
+        quote_index = random.randrange(0, len(quotes))
+    last_quote = quote_index
 
-    if len(colors) > 1 and last_color is not None: # Choosing a random color
-        while color_index == last_color: # Active when the data is pulled and is the same as the last pull
-            color_index = random.randrange(0, len(colors)) # Forces another pull from the feed to prevent the same color from displaying twice
+    # Choose a random color from colors
+    if len(colors) > 1 and last_color is not None:
+        while color_index == last_color:
+            color_index = random.randrange(0, len(colors))
     else:
-        color_index = random.randrange(0, len(colors)) # Used if the same color isn't pulled and runs normally
-    last_color = color_index  # Telling us that a quote has be chosen and not to pick it twice by changing the value of last_quote to it
+        color_index = random.randrange(0, len(colors))
+    last_color = color_index
+    
+     if len(anims) > 1 and last_quote is not None:
+        while anim_index == anim_state:
+            anim_index = random.randrange(0, len(anims))
+    else:
+        anim_index = random.randrange(0, len(ani,s))
+    animstate = anim_index
 
-    # Set the quote text
-    matrixportal.set_text(quotes[quote_index])
+    if anims == 1: 
+        SCROLL_DELAY = 0.00
+        matrixportal.set_text( " " + quotes[quote_index])
+        time.sleep(1)
+        matrixportal.set_text( "|" + quotes[quote_index])
+        time.sleep(1)
+        matrixportal.set_text( " " + quotes[quote_index])
+    else if anims == 2: 
+        SCROLL_DELAY = 0.01
+        matrixportal.set_text( ">" + quotes[quote_index])
+        time.sleep(4)
+        SCROLL_DELAY = -0.01
+        matrixportal.set_text( "<" + quotes[quote_index])
+    else if anims == 3: 
+        SCROLL_DELAY = 0.00
+        quotes.replace("A", "4"),   quotes.replace("a", "4")
+        quotes.replace("E", "3"),   quotes.replace("e", "3")
+        quotes.replace("I", "1"),   quotes.replace("i", "1")
+        quotes.replace("N", "|\|"),   quotes.replace("W", "\/\/")
+        time.sleep(1)
+        matrixportal.set_text(quotes[quote_index]) 
+    else if anims == 4: 
+        SCROLL_DELAY = 0.01
+        matrixportal.set_text(quotes[quote_index])
+        quotes.replace(quotes, "xxxxxx")
+        matrixportal.set_text(quotes[quote_index])
+        time.sleep(2)
+        quotes.replace(quotes, last_quote)
+        matrixportal.set_text(quotes[quote_index])
+    else 
+        SCROLL_DELAY = 0.02
+        matrixportal.set_text(quotes[quote_index])
+    ANIM_STAGE = -2 
 
     # Set the text color
     matrixportal.set_text_color(colors[color_index])
@@ -87,6 +153,6 @@ while True:
     # Scroll it
     matrixportal.scroll_text(SCROLL_DELAY)
 
-    if time.monotonic() > last_update + UPDATE_DELAY: # Restart the delay to re-run the function
-        update_data() # Running the updating function
-        last_update = time.monotonic() # Updating time to monotonic so scrolling and delays are consistent
+    if time.monotonic() > last_update + UPDATE_DELAY:
+        update_data()
+        last_update = time.monotonic()
